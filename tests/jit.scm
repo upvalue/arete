cde1d0966a4033b50e2c60851ec93ce2b7c0b43c
Phil <phil@upvalue.io>
2017-12-28 22:01:03 -0600

    Native code refactor.
    - Use stack frame pointer instead of stack frame index. Fewer
      instructions required, less mental overhead.
    - Add space in the stack frame for closure pointer, spare pointer.
    - Native code now calls procedure_addr!

--- tests/jit.scm
+++ tests/jit.scm
@@ -4,6 +4,7 @@
   (lambda (x)
     #`(let ()
         (define fn ,(caddr x))
+        (print ";; compiling function" (quote ,(caddr x)))
         (define result
           (try (lambda () (apply vmfunction->native (cons fn (quote ,(cdddr x)))))
                (lambda (exc) ''exception)))
@@ -22,13 +23,13 @@
 (test-equals #t (lambda () #t))
 (test-equals "asdf" (lambda () #t "asdf"))
 (test-equals 123 (lambda () global))
+(test-equals 124 (lambda () (set! global 124) global))
 
 (test-equals 4 (lambda () (fx+ 2 2)))
 
 (test-equals 423 (lambda (a) a) 423)
 (test-equals 10 (lambda (a b) (fx+ a b)) 5 5)
 (test-equals 723 (lambda (a) (set! a 723) a) #f)
-(test-equals 824 (lambda () (set! global 824) global))
 
 (test-equals 5
              (lambda ()
@@ -47,5 +48,12 @@
 (define (return-exc) (raise 'asdf "asdf" #t))
 
 (test-equals 'exception (lambda () (return-exc)))
+
+(define (return-arg a) a)
+
+(test-equals 2 (lambda () (return-arg 2)))
+(test-equals 2 (lambda () (return-arg 2) (return-arg 2) (return-arg 2)))
+
+(test-equals 'exception (lambda () (2)))
 
 (for-each print test-results)
