70565c74a53230dba69a074a5131b8b76e3437f8
Phil <phil@upvalue.io>
2017-12-28 23:35:09 -0600

    Native: add fx<

---
+++ examples/fib-fast.scm
@@ -0,0 +1,8 @@
+;; fib-fast.scm - uses 2-argument fixnum builtins
+
+(define (fib-fast n)
+  (if (fx< n 2) n (fx+ (fib-fast (fx- n 1)) (fib-fast (fx- n 2)))))
+
+(display (fib-fast 1))
+(newline)
+    
